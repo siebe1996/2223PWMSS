aefedaea0574e395aa2b743b8aac1bc86c552bac
Siebe Van de Voorde <siebe.vandevoorde@student.odisee.be>
2022-11-28 16:44:39 +0100

    added bramus router

--- app/public/php/index.php
+++ app/public/php/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once ('../../vendor/autoload.php');
+require_once ('../../config/database.php');
 
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../../resources/templates');
 $twig = new \Twig\Environment($loader, [
