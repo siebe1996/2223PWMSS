d650e9eabb9be7d6b259f2250ea5060b6651e519
Bert Van den Broeck <bert.vandenbroeck@student.odisee.be>
2023-01-04 14:26:44 +0100

    account pagina met info over db

--- app/src/Controllers/TripController.php
+++ app/src/Controllers/TripController.php
@@ -26,31 +26,25 @@ class TripController
             exit();
         }
 
+        print_r($_SESSION);
+        print_r($_GET);
+
+        $stmt = $this->conn->prepare('SELECT * FROM trips WHERE costumer_id = ?;');
+        $result = $stmt->executeQuery([$_SESSION['user']['id']]);
+        $trips = $result->fetchAllAssociative();
+
+        print PHP_EOL;
+        print_r($trips);
+        print PHP_EOL;
+
         echo $this->twig->render('pages/account.twig', [
             'loggedIn' => true,
             'User' => [
-                'name' => 'Lukas Downes',
-                'email' => 'lukasdownes@gmail.com',
+                'name' => $_SESSION['user']['first_name'] . ' ' . $_SESSION['user']['last_name'],
+                'email' => $_SESSION['user']['email'],
                 'status' => 'Rider',
-                'rideAmount' => 27,
-                'rideHistory' => [
-                    [
-                        'day' => 'Zondag',
-                        'time' => 17,
-                        'from' => 'Anderlecht',
-                        'to' => 'Gent',
-                        'date' => '12/12/\'2022',
-                        'cost' => 7.63
-                    ],
-                    [
-                        'day' => 'Zondag',
-                        'time' => 10,
-                        'from' => 'Zaventem',
-                        'to' => 'Anderlecht',
-                        'date' => '12/12/\'2022',
-                        'cost' => 12.9
-                    ]
-                ]
+                'rideAmount' => count($trips),
+                'rideHistory' => $trips
             ]
         ]);
     }
