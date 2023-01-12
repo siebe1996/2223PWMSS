ed5e88889c5277b0fb99720bb889cc9a9a299839
Bert Van den Broeck <bert.vandenbroeck@student.odisee.be>
2023-01-12 11:06:55 +0100

    overgezet naar .env

--- app/config/email.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// smtp://username:password@smtp.example.com:port
-
-define('USERNAME', '6794ff7266d768');
-define('PASSWORD', '096c39151ea105');
-define('HOST', 'smtp.mailtrap.io');
-define('PORT', '2525');
-define('OPTIONS', 'encryption=tls&auth_mode=login');
