e83fdcf9cd567b577ca2889600a48ddca7fcb64c
Siebe Van de Voorde <siebe.vandevoorde@student.odisee.be>
2023-01-12 18:13:25 +0100

    confomatie pagina aangemaakt

--- app/src/Controllers/TripController.php
+++ app/src/Controllers/TripController.php
@@ -42,7 +42,7 @@ class TripController
                 status
             FROM trips as t 
             WHERE t.driver_id = ? AND (t.status = "claimed" OR t.status = "started")
-            ORDER BY t.time
+            ORDER BY t.start_time
             SQL,
             [$_SESSION['user']['id']]
         );
@@ -59,7 +59,7 @@ class TripController
                 id
             FROM trips as t 
             WHERE t.status = "pending"
-            ORDER BY t.time
+            ORDER BY t.start_time
             SQL
         );
 
@@ -73,60 +73,82 @@ class TripController
         ]);
     }
 
+    public function confirm(){
+        $tripId = $_POST['accept'] ?? '';
+        $_SESSION['tripId'] = $tripId;
+        header('Location: /driver/rides/'. urlencode($tripId) .'/confirm');
+        exit();
+    }
+
+    public function showConfirm(){
+        echo $this->twig->render('pages/acceptRide.twig', [
+            'rideId' => $_SESSION['tripId'],
+        ]);
+    }
+
     public function acceptRide()
     {
-        $userId = $_SESSION['user']['id'];
-        $tripId = $_POST['accept'] ?? '';
-        if (!trim($tripId) || !trim($userId)) {
-            header('location:/badrequest');
-            exit();
-        } else {
-            $stmt = $this->conn->prepare('SELECT * FROM trips WHERE status = ? AND id = ?');
-            $result = $stmt->executeQuery(['pending', $tripId]);
-            $trip = $result->fetchAssociative();
-
-            if ($trip) {
-                $stmt = $this->conn->prepare('UPDATE trips SET status = ?, driver_id = ? WHERE id = ?');
-                $result = $stmt->executeStatement(['claimed', $userId, $tripId]);
-
-                $stmt = $this->conn->prepare('SELECT * FROM anonymous_users WHERE id = ?');
-                $result = $stmt->executeQuery([$trip['driver_id']]);
-                $driver = $result->fetchAssociative();
-
-                MailService::send(
-                    $this->twig,
-                    'info@rebu.be',
-                    $driver['email'],
-                    'Je hebt een rit geacepteerd',
-                    '',
-                    'email/acceptRide',
-                    [
-                        'first_name' => $driver['first_name'],
-                        'last_name' => $$driver['last_name'],
-                        'trip' => $trip,
-                        'driver' => true
-                    ]
-                );
-                MailService::send(
-                    $this->twig,
-                    'info@rebu.be',
-                    $_SESSION['user']['email'],
-                    'Je rit is geacepteerd',
-                    '',
-                    'email/acceptRide',
-                    [
-                        'first_name' => $_SESSION['user']['first_name'],
-                        'last_name' => $_SESSION['user']['last_name'],
-                        'trip' => $trip,
-                        'driver' => false
-                    ]
-                );
-
-                header('location:driver/rides');
+        if (isset($_POST['accept'])){
+            $userId = $_SESSION['user']['id'];
+            $tripId = $_SESSION['tripId'] ?? '';
+            unset($_SESSION['tripId']);
+            if (!trim($tripId) || !trim($userId)) {
+                //toDo denk hierover na
+                header('location:/badrequest');
                 exit();
+            } else {
+                $stmt = $this->conn->prepare('SELECT * FROM trips WHERE status = ? AND id = ?');
+                $result = $stmt->executeQuery(['pending', $tripId]);
+                $trip = $result->fetchAssociative();
+                if ($trip) {
+                    $stmt = $this->conn->prepare('UPDATE trips SET status = ?, driver_id = ? WHERE id = ?');
+                    $result = $stmt->executeStatement(['claimed', $userId, $tripId]);
+
+                    $stmt = $this->conn->prepare('SELECT * FROM anonymous_users WHERE id = ?');
+                    $result = $stmt->executeQuery([$userId]);
+                    $driver = $result->fetchAssociative();
+                    var_dump($driver);
+                    MailService::send(
+                        $this->twig,
+                        'info@rebu.be',
+                        $driver['email'],
+                        'Je hebt een rit geacepteerd',
+                        '',
+                        'email/acceptRide',
+                        [
+                            'first_name' => $driver['first_name'],
+                            'last_name' => $driver['last_name'],
+                            'trip' => $trip,
+                            'driver' => true
+                        ]
+                    );
+                    MailService::send(
+                        $this->twig,
+                        'info@rebu.be',
+                        $_SESSION['user']['email'],
+                        'Je rit is geacepteerd',
+                        '',
+                        'email/acceptRide',
+                        [
+                            'first_name' => $_SESSION['user']['first_name'],
+                            'last_name' => $_SESSION['user']['last_name'],
+                            'trip' => $trip,
+                            'driver' => false
+                        ]
+                    );
+
+                    header('location:driver/rides');
+                    exit();
+                }else{
+                    //toDo error raide is claimed
+                    header('location:/rideIsClaimed');
+                    exit();
+                }
             }
-            //toDo error raide is claimed
-            header('location:/rideIsClaimed');
+        }
+        else{
+            unset($_SESSION['tripId']);
+            header('location:/');
             exit();
         }
     }
@@ -145,13 +167,13 @@ class TripController
             $status = 'started';
         }
         if (!trim($tripId) || !trim($userId)) {
-            header('Location: badrequest');
+            header('location:/badrequest');
             exit();
         } else {
             $stmt = $this->conn->prepare('UPDATE trips SET status = ?, driver_id = ? WHERE id = ?');
             $result = $stmt->executeStatement([$status, $userId, $tripId]);
 
-            header('Location: driver/rides');
+            header('location:/driver/rides');
             exit();
         }
     }
@@ -162,14 +184,14 @@ class TripController
             $tripId = $_POST['cancel'] ?? '';
         }
         if (!trim($tripId)){
-            header('Location: badrequest');
+            header('location:/badrequest');
             exit();
         }
         else{
             $stmt = $this->conn->prepare('UPDATE trips SET status =  "cancelled" WHERE id = ?');
             $result = $stmt->executeStatement([$tripId]);
 
-            header('Location: /users');
+            header('location:/users');
             exit();
         }
     }
